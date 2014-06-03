8b2232589c3176f6eb0788f6dae4a527c818d8ea
TimePath <andrew.hardaker1995@gmail.com>
2014-06-03 15:56:54 +1000

    Implement basic security

--- src/main/java/com/timepath/major/Main.java
+++ src/main/java/com/timepath/major/Main.java
@@ -1,12 +1,18 @@
 package com.timepath.major;
 
-import com.timepath.major.proto.Addressbook.AddressBook;
-import com.timepath.major.proto.Addressbook.Person;
+import com.timepath.major.vfs.DatabaseConnection;
+import com.timepath.major.vfs.SecurityAdapter;
+import com.timepath.major.vfs.SecurityController;
+import com.timepath.vfs.SimpleVFile;
+import com.timepath.vfs.ftp.FTPFS;
+import com.timepath.vfs.jdbc.JDBCFS;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.channels.SocketChannel;
+import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -15,10 +21,38 @@ public class Main {
     private static final Logger LOG = Logger.getLogger(Main.class.getName());
 
     public static void main(String[] args) throws Exception {
+        if(args.length == 0) args = new String[] { "jdbc:mysql://localhost/test" };
+        if(args.length >= 2) {
+            Class.forName(args[1]);
+        }
+        final JDBCFS jdbcfs = new DatabaseConnection(args[0]);
+        FTPFS ftp = new FTPFS();
+        ftp.add(new SecurityAdapter(jdbcfs, new SecurityController() {
+            String user = System.getProperty("user.name");
+
+            @Override
+            public InputStream openStream(final SimpleVFile file) {
+                if(Math.round(Math.random()) == 1) return new ByteArrayInputStream("Go away".getBytes());
+                return super.openStream(file);
+            }
+
+            @Override
+            public Collection<? extends SimpleVFile> list(final SimpleVFile file) {
+                return file.list();
+            }
+
+            @Override
+            public SimpleVFile get(final SimpleVFile file) {
+                LOG.log(Level.INFO, "{0} is accessing {1}", new Object[] { user, file });
+                return file;
+            }
+        }));
+        new Thread(ftp).start();
         new AbstractServer(9001) {
             {
                 LOG.log(Level.INFO, "Starting server on port {0}", port);
             }
+
             @Override
             void connected(SocketChannel client) throws IOException {
                 InputStream is = client.socket().getInputStream();
